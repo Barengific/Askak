e98459d9f535bfbfd6ff1d16f1a4da2d64ed0b72
Barengific <75044077+Barengific@users.noreply.github.com>
2021-01-21 22:40:09 +0000

    Stamina
    Added Stamina GUI bar and toggle. Not working properly

---
+++ Askak/Assets/scripts/Stamina.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stamina : MonoBehaviour{
+    // Start is called before the first frame update
+    [SerializeField]
+    private int maxStamina = 100;
+
+    public int currentStamina;
+
+    public Toggle toggle;
+
+    public event Action<float> OnStaminaPctChanged = delegate{ };
+
+    private void OnEnable()
+    {
+        currentStamina = maxStamina;
+    }
+
+    public void ModifyStamina(int amount)
+    {
+        currentStamina += amount;
+
+        float currentStaminaPct = (float)currentStamina / (float)maxStamina;
+        OnStaminaPctChanged(currentStaminaPct);
+    }
+
+    //private IEnumerator HandleStamina()
+    //{
+    //    //fore.fillAmount = pct;
+    //    //StartCoroutine(ChangeToPct(pct));
+    //    while (toggle.isOn)
+    //    {
+    //        //if (toggle.isOn)    {
+    //            ModifyStamina(-10);
+    //            yield return new WaitForSeconds(2);
+    //    //}
+    //    }
+
+    //}
+    void Start() {
+        //StartCoroutine(HandleStamina());
+
+    }
+    // Update is called once per frame
+    private void Update()    {
+        //StartCoroutine(HandleStamina(1f));
+        //HandleStamina(1f);
+
+        if (toggle.isOn)       {
+            ModifyStamina(-10);
+            //Debug.Log("tooge on");
+            
+        }
+        Debug.Log(toggle.isOn);
+    }
+}
---
+++ Askak/Assets/scripts/StaminaBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Image StaminaImage;
+    [SerializeField]
+    private float updateSpeedsSeconds = 5f;
+
+    private void Awake()
+    {
+        //foregroundImage = GetComponent<Image>();
+        GetComponentInParent<Stamina>().OnStaminaPctChanged += HandleStaminaChanged;
+    }
+
+    private void HandleStaminaChanged(float pct)
+    {
+        //fore.fillAmount = pct;
+        StartCoroutine(ChangeToPct(pct));
+    }
+
+    private IEnumerator ChangeToPct(float pct)
+    {
+        float preChangepct = StaminaImage.fillAmount;
+        float elapsed = 0f;
+        while(elapsed < updateSpeedsSeconds){
+            elapsed += Time.deltaTime;
+            StaminaImage.fillAmount = Mathf.Lerp(preChangepct, pct, elapsed / updateSpeedsSeconds);
+            yield return null;
+            yield return new WaitForSeconds(2);
+        }
+        StaminaImage.fillAmount = pct;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+    }
+}
