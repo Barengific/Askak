1a8c9f6557ae269a2885f34a1388406e8ee8d734
Barengific <75044077+Barengific@users.noreply.github.com>
2021-01-21 19:26:24 +0000

    Health
    Health Bar GUI and decrease when space button is pressed

---
+++ Askak/Assets/scripts/HealthBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Image foregroundImage;
+    [SerializeField]
+    private float updateSpeedsSeconds = 0.5f;
+
+    private void Awake()
+    {
+        //foregroundImage = GetComponent<Image>();
+        GetComponentInParent<Health>().OnHealthPctChanged += HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(float pct)
+    {
+        //fore.fillAmount = pct;
+        StartCoroutine(ChangeToPct(pct));
+    }
+
+    private IEnumerator ChangeToPct(float pct)
+    {
+        float preChangepct = foregroundImage.fillAmount;
+        float elapsed = 0f;
+        while(elapsed < updateSpeedsSeconds){
+            elapsed += Time.deltaTime;
+            foregroundImage.fillAmount = Mathf.Lerp(preChangepct, pct, elapsed / updateSpeedsSeconds);
+            yield return null;
+        }
+        foregroundImage.fillAmount = pct;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+    }
+}
